9b7ee7f8fe2d82059fdc96b53cf435cad5f7ad92
NoaHod1 <noa272007@gmail.com>
2025-01-03 09:58:30 +0200

    EOD 3.1.25

---
+++ myProjectNoaH/app/src/main/java/com/noa/eatandshare/models/User.java
@@ -0,0 +1,91 @@
+package com.noa.eatandshare.models;
+
+public class User {
+    String id;
+    String fname, lname, phone,email, password,city;
+
+    public User(String uid, String email, String password, String fName, String lName, String phone) {
+    }
+
+
+    public User(String id, String fname, String lname, String phone, String email, String password, String city) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+        this.city=city;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
+
---
+++ myProjectNoaH/app/src/main/java/com/noa/eatandshare/screens/Login.java
@@ -0,0 +1,106 @@
+package com.noa.eatandshare.screens;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.noa.eatandshare.R;
+import com.noa.eatandshare.models.User;
+import com.noa.eatandshare.services.AuthenticationService;
+import com.noa.eatandshare.services.DatabaseService;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etEmail, etPassword;
+    Button btnLog;
+    String email, pass;
+    AuthenticationService authenticationService;
+    DatabaseService databaseService;
+
+    public static final String MyPREFERENCES = "MyPrefs";
+
+    SharedPreferences sharedpreferences;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        initViews();
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        email = sharedpreferences.getString("email", "");
+        pass = sharedpreferences.getString("password", "");
+        etEmail.setText(email);
+        etPassword.setText(pass);
+    }
+
+
+    private void initViews() {
+        etEmail = findViewById(R.id.etEmailLogin);
+        etPassword = findViewById(R.id.etPasswordLogin);
+        btnLog = findViewById(R.id.btnLogin);
+        btnLog.setOnClickListener(this);
+
+
+        authenticationService = AuthenticationService.getInstance();
+        databaseService = DatabaseService.getInstance();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        email = etEmail.getText().toString();
+        pass = etPassword.getText().toString();
+
+        authenticationService.signIn(email, pass, new AuthenticationService.AuthCallback<String>() {
+            @Override
+            public void onCompleted(String id) {
+                Log.d("TAG", "signInWithEmail:success");
+
+                databaseService.getUser(id, new DatabaseService.DatabaseCallback<User>() {
+                    @Override
+                    public void onCompleted(User user) {
+
+                        Intent go = new Intent(getApplicationContext(), HomePage.class);
+                        startActivity(go);
+                    }
+
+                    @Override
+                    public void onFailed(Exception e) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onFailed(Exception e) {
+                Log.w("TAG", "signInWithEmail:failure", e);
+                Toast.makeText(getApplicationContext(), "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
