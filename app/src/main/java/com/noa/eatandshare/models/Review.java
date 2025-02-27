dc63058014fe9375354f2f2a0bc4a7e6ce694c7b
NoaHod1 <noa272007@gmail.com>
2025-02-27 12:23:03 +0200

    EOD 6.3.25

--- app/src/main/java/com/noa/eatandshare/models/Review.java
+++ app/src/main/java/com/noa/eatandshare/models/Review.java
@@ -1,11 +1,75 @@
 package com.noa.eatandshare.models;
 
 public class Review {
-    int id;
+
+    Restaurant restaurant;
     User user;
     String date;
     double rate;
 
+    String details;
 
 
+
+    public Review() {
+    }
+
+    public Review(Restaurant restaurant, User user, String date, double rate, String details) {
+        this.restaurant = restaurant;
+        this.user = user;
+        this.date = date;
+        this.rate = rate;
+        this.details = details;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "restaurant=" + restaurant +
+                ", user=" + user +
+                ", date='" + date + '\'' +
+                ", rate=" + rate +
+                ", details='" + details + '\'' +
+                '}';
+    }
 }
--- app/src/main/java/com/noa/eatandshare/screens/RestaurantProfile.java
+++ app/src/main/java/com/noa/eatandshare/screens/RestaurantProfile.java
@@ -1,6 +1,11 @@
 package com.noa.eatandshare.screens;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +14,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.noa.eatandshare.R;
+import com.noa.eatandshare.adapters.ReviewsAdapter;
+import com.noa.eatandshare.models.Review;
+
+import java.util.ArrayList;
 
 public class RestaurantProfile extends AppCompatActivity {
+
+
+    TextView tvResName, tvType, tvKosher, tvCity, tvDomain;
+    ImageView ivRes;
+    RatingBar rb;
+    Button btnAdd, btnSave, btnBack;
+    ListView lvReviews;
+    ReviewsAdapter reviewsAdapter;
+    ArrayList<Review> reviewArratyList=new ArrayList();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +41,26 @@ public class RestaurantProfile extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initViews();
+
+    }
+
+    private void initViews()
+
+    {
+        tvResName=findViewById(R.id.tvNameResProfile);
+        tvType=findViewById(R.id.tvTypeResProfile);
+        tvKosher=findViewById(R.id.tvKosherResProfile);
+        tvCity=findViewById(R.id.tvCityResProfile);
+        tvDomain=findViewById(R.id.tvDomainResProfile);
+        ivRes=findViewById(R.id.ivResProfile);
+        rb=findViewById(R.id.ratingBarResProfile);
+        btnSave=findViewById(R.id.btnSaveResProfile);
+        btnBack=findViewById(R.id.btnBackResProfile);
+        lvReviews=findViewById(R.id.);
+
+
+
     }
 }
